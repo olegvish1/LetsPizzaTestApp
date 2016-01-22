7aca0e955ac3d06adc6fd7209fb64435353f420c
Oleg Vishnivetskiy <olegvish81@gmail.com>
2016-01-22 13:12:29 +0200

    Initial Commit

---
+++ LetsPizza/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LetsPizza
+//
+//  Created by Oleg Vishnivetskiy on 22/01/16.
+//  Copyright © 2016 Oleg Vishnivetskiy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
